ffd12419765c57e0ed3eabc86342d4a668d5b4ed
lucicreando <luicelibuenosaites2019@gmail.com>
2023-09-08 22:52:23 -0300

    act3

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = document.getElementById('carouselExampleCaptions');
+    var carousel = new bootstrap.Carousel(myCarousel);
+  });
+</script>
